e92327ba27fe745f0cad8b7917013ac36efb85ef
kaustavhalder <khalder.08@gmail.com>
2024-03-07 11:20:09 +0530

    compute added

---
+++ compute/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("../pvt/key.json")
+
+  project = "centering-brook-408512"
+  region  = "asia-south1"
+  zone    = "asia-south1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
